ea34f69d9e96af41fad6e83e54c109155ddc98eb
Abhishek kumar verma <abhishekverma95100@gmail.com>
2025-09-01 20:49:24 +0530

    h

---
+++ 10_object.py/1problem.py
@@ -0,0 +1,13 @@
+class programmer:
+    company = "Microsoft"
+    def __init__(self,name ,salary,pin):
+        self.name = name
+        self.salary = salary
+        self.pin = pin
+
+
+        
+p = programmer("Abhishek", 12000000, 1234)
+
+print(p.name,p.salary,p.pin,p.company)
+
